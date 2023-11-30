679cfa5c701853a7d9dce1b45d16406ccae8b8fd
mununki <woonki.moon@gmail.com>
2023-11-30 15:43:38 +0900

    map str for basic

--- src/ppx/structure.ml
+++ src/ppx/structure.ml
@@ -1,9 +1,205 @@
 open Ppxlib
 open Parsetree
-(* open Ast_helper *)
-(* open Utils *)
+open Ast_helper
+open Utils
+
+let map_type_decl
+    {
+      ptype_attributes;
+      ptype_name = { txt };
+      ptype_manifest;
+      ptype_kind;
+      ptype_loc;
+    } =
+  if List.exists has_attribute ptype_attributes then
+    match (ptype_manifest, ptype_kind) with
+    | None, Ptype_record lds ->
+        let fields = lds |> List.map (fun { pld_name = { txt } } -> txt) in
+        let type_decls =
+          Str.type_ Recursive
+            [
+              (* type useFormReturnOfInputs = {
+                   register: (variantOfInputs, ~options: registerOptionsOfInputs=?) => JsxDOM.domProps,
+                   handleSubmit: (inputs => unit) => JsxEvent.Form.t => unit,
+                   watch: variantOfInputs => watchReturnOfInputs,
+                   formState: formStateOfInputs,
+                 } *)
+              Type.mk
+                (mkloc
+                   ("useFormReturnOf" ^ String.capitalize_ascii txt)
+                   ptype_loc)
+                ~priv:Public
+                ~kind:
+                  (Ptype_record
+                     [
+                       (* register: (variantOfInputs, ~options: registerOptionsOfInputs=?) => JsxDOM.domProps, *)
+                       Type.field ~mut:Immutable (mknoloc "register")
+                         (uncurried_core_type_arrow ~arity:2
+                            [
+                              Typ.arrow Nolabel
+                                (Typ.constr
+                                   (lid @@ "variantOf"
+                                   ^ String.capitalize_ascii txt)
+                                   [])
+                                (Typ.arrow (Optional "options")
+                                   (Typ.constr
+                                      ~attrs:
+                                        [
+                                          Attr.mk
+                                            (mknoloc "res.namedArgLoc")
+                                            (PStr []);
+                                        ]
+                                      (lid @@ "registerOptionsOf"
+                                      ^ String.capitalize_ascii txt)
+                                      [])
+                                   (Typ.constr
+                                      (mknoloc
+                                         (Longident.Ldot
+                                            (Lident "JsxDOM", "domProps")))
+                                      []));
+                            ]);
+                       (* handleSubmit: (inputs => unit) => JsxEvent.Form.t => unit, *)
+                       Type.field ~mut:Immutable (mknoloc "handleSubmit")
+                         (uncurried_core_type_arrow ~arity:1
+                            [
+                              Typ.arrow Nolabel
+                                (uncurried_core_type_arrow ~arity:1
+                                   [
+                                     Typ.arrow Nolabel
+                                       (Typ.constr (lid txt) [])
+                                       (Typ.constr (lid "unit") []);
+                                   ])
+                                (uncurried_core_type_arrow ~arity:1
+                                   [
+                                     Typ.arrow Nolabel
+                                       (Typ.constr
+                                          (mknoloc
+                                             (Longident.Ldot
+                                                ( Ldot
+                                                    (Lident "JsxEvent", "Form"),
+                                                  "t" )))
+                                          [])
+                                       (Typ.constr (lid "unit") []);
+                                   ]);
+                            ]);
+                       (* watch: variantOfInputs => watchReturnOfInputs, *)
+                       Type.field ~mut:Immutable (mknoloc "watch")
+                         (uncurried_core_type_arrow ~arity:1
+                            [
+                              Typ.arrow Nolabel
+                                (Typ.constr
+                                   (lid @@ "variantOf"
+                                   ^ String.capitalize_ascii txt)
+                                   [])
+                                (Typ.constr
+                                   (lid @@ "watchReturnOf"
+                                   ^ String.capitalize_ascii txt)
+                                   []);
+                            ]);
+                       (* formState: formStateOfInputs, *)
+                       Type.field ~mut:Immutable (mknoloc "formState")
+                         (Typ.constr
+                            (lid @@ "formStateOf" ^ String.capitalize_ascii txt)
+                            []);
+                     ]);
+              (* type variantOfinputs = | @as("example") Example | @as("exampleRequired") ExampleRequired *)
+              Type.mk
+                (mkloc ("variantOf" ^ String.capitalize_ascii txt) ptype_loc)
+                ~priv:Public
+                ~kind:(Ptype_variant (make_const_decls fields ptype_loc));
+              (* type registerOptionsOfInputs = {required?: bool} *)
+              Type.mk
+                (mkloc
+                   ("registerOptionsOf" ^ String.capitalize_ascii txt)
+                   ptype_loc)
+                ~priv:Public
+                ~kind:
+                  (Ptype_record
+                     [
+                       Type.field ~mut:Immutable (mknoloc "required")
+                         (Typ.constr
+                            ~attrs:
+                              [ Attr.mk (mknoloc "res.optional") (PStr []) ]
+                            (lid "bool") []);
+                     ]);
+              (* @unboxed type watchReturnOfInputs = String(string) | Number(float) *)
+              Type.mk
+                (mkloc
+                   ("watchReturnOf" ^ String.capitalize_ascii txt)
+                   ptype_loc)
+                ~attrs:[ Attr.mk (mknoloc "unboxed") (PStr []) ]
+                ~priv:Public
+                ~kind:
+                  (Ptype_variant
+                     [
+                       Type.constructor (mknoloc "String")
+                         ~args:(Pcstr_tuple [ Typ.constr (lid "string") [] ]);
+                       Type.constructor (mknoloc "Number")
+                         ~args:(Pcstr_tuple [ Typ.constr (lid "float") [] ]);
+                     ]);
+              (* type formStateOfInputs = {errors: fieldErrorsOfInputs} *)
+              Type.mk
+                (mkloc ("formStateOf" ^ String.capitalize_ascii txt) ptype_loc)
+                ~priv:Public
+                ~kind:
+                  (Ptype_record
+                     [
+                       Type.field ~mut:Immutable (mknoloc "errors")
+                         (Typ.constr
+                            (lid @@ "fieldErrorsOf"
+                            ^ String.capitalize_ascii txt)
+                            []);
+                     ]);
+              (* type fieldErrorsOfInputs = { example: bool, exampleRequired: bool } *)
+              Type.mk
+                (mkloc
+                   ("fieldErrorsOf" ^ String.capitalize_ascii txt)
+                   ptype_loc)
+                ~priv:Public
+                ~kind:
+                  (Ptype_record
+                     (lds
+                     |> List.map (fun (ld : label_declaration) ->
+                            {
+                              ld with
+                              pld_type = Typ.constr (lid "bool") [];
+                              pld_attributes =
+                                remove_optional_attribute ld.pld_attributes;
+                            })));
+            ]
+        in
+        (* @module("react-hook-form")
+           external useFormOfInputs: unit => useFormReturnOfInputs = "useForm" *)
+        let primitive_use_form =
+          Str.primitive
+            (Val.mk
+               ~attrs:
+                 [
+                   Attr.mk (mknoloc "module")
+                     (PStr
+                        [
+                          Str.eval
+                          @@ Exp.constant (Const.string "react-hook-form");
+                        ]);
+                 ]
+               ~prim:[ "useForm" ]
+               (mknoloc @@ "useFormOf" ^ String.capitalize_ascii txt)
+               (uncurried_core_type_arrow ~arity:1
+                  [
+                    Typ.arrow Nolabel
+                      (Typ.constr (lid "unit") [])
+                      (Typ.constr
+                         (lid @@ "useFormReturnOf" ^ String.capitalize_ascii txt)
+                         []);
+                  ]))
+        in
+        [ type_decls; primitive_use_form ]
+    | _ -> fail ptype_loc "This type is not handled by @ppx_ts.keyOf"
+  else []
 
 let map_structure_item mapper ({ pstr_desc } as structure_item) =
   match pstr_desc with
-  | Pstr_type (_, _) -> [ mapper#structure_item structure_item ]
+  | Pstr_type (_, decls) ->
+      let structure_items = decls |> List.map map_type_decl |> List.concat in
+      mapper#structure_item structure_item :: structure_items
   | _ -> [ mapper#structure_item structure_item ]
